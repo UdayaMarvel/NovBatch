48eb7e0e4eb7b742d9ce4eda2061e278f3c732aa
tivagar <tivagar0808@gmail.com>
2022-12-23 16:58:26 +0530

    add program

--- src/test/java/org/gitHub/GitClass.java
+++ src/test/java/org/gitHub/GitClass.java
@@ -1,8 +1,20 @@
 package org.gitHub;
 
 public class GitClass {                                                                                                                                                                                                                                                           
-	
-public static void main(String[] args) {
-	System.out.println("test");
-}
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4,5,6,7,8,9,10};
+		double sum = 0;
+		
+		for (int i=0; i <a.length; i++) {
+			sum = sum + a[i];
+			
+			
+		}
+		System.out.println("sum = "+sum);
+		double avg = sum / 10;
+		System.out.println(avg);
+		// TODO Auto-generated method stub
+
+	}
+
 }
