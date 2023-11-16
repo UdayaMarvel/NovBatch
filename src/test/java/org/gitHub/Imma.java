6bceb3e4c49c8debbcb75f97ae502da949b5cb12
immarani <imimmarani12@gmail.com>
2023-11-16 12:35:33 +0530

    add new program

---
+++ src/test/java/org/gitHub/Imma.java
@@ -0,0 +1,16 @@
+package org.gitHub;
+
+public class Imma {
+
+	public static void main(String[] args) {
+		
+		String s="welcome to project session";
+		String[] st=s.split(" ");
+		for(int i=0;i<st.length;i++) {
+		
+			System.out.println(st[i]);
+		}
+
+	}
+
+}
